45ad3ad2035251cc0752e4953dfc7491930acc5a
John Fox <J.Arcoudis@gmail.com>
2021-10-04 18:45:30 +1100

    Working Sync Func

--- sync.js
+++ sync.js
@@ -1,15 +1,13 @@
-// TODO: Add List of Files to sync
-// TODO: Add Ability to specify what file to sync
-// TODO: Add Ability to sync all files
-const scripts = ['skynet.js', ]
+const scripts = ['skynet.js'];
+const url = 'https://raw.githubusercontent.com/MrFoxington/Bitburner/master/';
+
 export async function main(ns) {
-    ns.tprint("Starting Sync...");
-    const url = 'https://raw.githubusercontent.com/MrFoxington/Bitburner/master/';
 
     if (ns.args.length == 0) {
         ns.tprint('Sync requires at least 1 argument');
         return;
     }
+    ns.tprint("Starting Sync...");
 
     if (ns.args[0] == 'all') {
         await download(ns, url, scripts);
@@ -19,7 +17,7 @@ export async function main(ns) {
 }
 
 async function download(ns, url, names) {
-    names.forEach(name => {
+    names.forEach(async name => {
         await ns.wget(url + name, name);
         ns.tprint("Sync Complete: " + name);
     });
