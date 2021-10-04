476ab4b7de7b91da7a8b796b09b24a240230176e
John Fox <J.Arcoudis@gmail.com>
2021-10-04 22:41:59 +1100

    Refactored to remove simultanius wget calls. Added Comments Added TODO

--- sync.js
+++ sync.js
@@ -1,6 +1,11 @@
-const scripts = ['skynet.js', 'test.js'];
+const scripts = ['skynet.js'];
 const url = 'https://raw.githubusercontent.com/MrFoxington/Bitburner/master/';
+// TODO: Downloading scripts from different Branches
+// Currently only master branch is available
 
+// Downloads scripts from GitHub.
+// Args: 'all' -> Downloads all available scripts
+// Args: Can specify specific scripts to download by name.
 export async function main(ns) {
 
     if (ns.args.length == 0) {
@@ -17,15 +22,18 @@ export async function main(ns) {
     ns.tprint("Finished Sync");
 }
 
+
+// Downloads a list of names scripts from GitHub Master branch
+// Built to handle async calls (BitBurner doesn't allow it)
 async function downloadList(ns, url, names) {
-    let promises = [];
     for (let i in names) {
         let name = names[i];
-        promises.push(download(ns, url + name, name));
+        await download(ns, url + name, name);
     }
-    await Promise.allSettled(promises);
 }
 
+// Downloads script from external website
+// Build to handle async calls (BitBurner doesn't allow it)
 async function download(ns, url, name) {
     return ns.wget(url, name).then(data => {
         let txt = ""
