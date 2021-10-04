8226c4a18aafd9079c2d85186e9cf137f184849b
John Fox <J.Arcoudis@gmail.com>
2021-10-04 22:00:38 +1100

    Added Promis handling and fucntionality to sync

--- sync.js
+++ sync.js
@@ -1,4 +1,4 @@
-const scripts = ['skynet.js'];
+const scripts = ['skynet.js', 'test.js'];
 const url = 'https://raw.githubusercontent.com/MrFoxington/Bitburner/master/';
 
 export async function main(ns) {
@@ -7,18 +7,29 @@ export async function main(ns) {
         ns.tprint('Sync requires at least 1 argument');
         return;
     }
-    ns.tprint("Starting Sync...");
 
+    ns.tprint("Starting Sync...");
     if (ns.args[0] == 'all') {
-        await download(ns, url, scripts);
+        await downloadList(ns, url, scripts);
     } else {
-        await download(ns, url, ns.args);
+        await downloadList(ns, url, ns.args);
     }
+    ns.tprint("Finished Sync");
 }
 
-async function download(ns, url, names) {
-    names.forEach(async name => {
-        await ns.wget(url + name, name);
-        ns.tprint("Sync Complete: " + name);
+async function downloadList(ns, url, names) {
+    let promises = [];
+    for (let i in names) {
+        let name = names[i];
+        promises.push(download(ns, url + name, name));
+    }
+    await Promise.allSettled(promises);
+}
+
+async function download(ns, url, name) {
+    return ns.wget(url, name).then(data => {
+        let txt = ""
+        data ? txt = "Synced: " : txt = "Failed: ";
+        ns.tprint(txt + name)
     });
 }
