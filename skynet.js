4bba3d88f69f9b4cbce30f4193c360426a2dd731
John Fox <J.Arcoudis@gmail.com>
2021-10-04 16:13:45 +1100

    Dumb loading text moved into function

--- skynet.js
+++ skynet.js
@@ -1,8 +1,12 @@
 export async function main(ns){
+    init();
+}
+
+function init() {
     ns.print('Initalizing Skynet...');
     ns.print('Loading... 25%');
     ns.print('Please prepare for extinction...');
     ns.print('Loading... 77%');
     ns.print('Skynet Initialization complete');
-    ns.print('Welcome to the New World Order!')  
+    ns.print('Welcome to the New World Order!')      
 }
