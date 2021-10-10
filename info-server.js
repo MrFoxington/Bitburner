d0a19a7bebb1eb2d9e25a7085319d2fcb9175818
John Fox <J.Arcoudis@gmail.com>
2021-10-10 15:21:48 +1100

    Updated info utill

--- info-server.js
+++ info-server.js
@@ -5,7 +5,8 @@ export async function main(ns){
         ns.tprint("Stat Readout for " + host);
         ns.tprint("Hacking Req: " + ns.getServerRequiredHackingLevel(host));
         ns.tprint("Hacking Ports: " + ns.getServerNumPortsRequired(host));
-        ns.tprint("Server Ram: " + ns.getServerRam(host));
+        ns.tprint("Server Max Ram: " + ns.getServerMaxRam(host));
+        ns.tprint("Server Used Ram: " + ns.getServerUsedRam(host));
         ns.tprint("Growth Rate: " + ns.getServerGrowth(host));
         ns.tprint("Hack %: " + ns.hackAnalyzePercent(host));
         ns.tprint("Max Money: " + ns.getServerMaxMoney(host));
